0e996e53bd4c63a2d46070ba8ee6316e73d54fee
Ruben Pinto <rpinto@spartaglobal.com>
2018-11-04 17:39:09 +0000

    v.6.2 Added form for the user comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,7 @@
+class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to post_path(@post)
+    end
+end
